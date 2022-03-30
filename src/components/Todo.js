f320b3636a30e02457e01e684c9ecd8c38b9e5f1
kiki014045@gmail.com <kiki014045@gmail.com>
2022-03-30 10:23:34 +0700

    fix sort data

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -10,15 +10,17 @@ const Todo = ({ dispatch, id, title, description, status, createdAt }) => {
   };
   return (
     <>
-      <li
-        onClick={() => setShowModal(!showModal)}
-        style={{
-          textDecoration: status === 1 ? "line-through" : "none",
-        }}
-      >
-        {title}
-        {status === 0 && (
-          <span className="inline ml-2">
+      <div className="flex flex-row">
+        <li
+          onClick={() => setShowModal(!showModal)}
+          style={{
+            textDecoration: status === 1 ? "line-through" : "none",
+          }}
+        >
+          <p className="hover:text-sky-700 cursor-pointer">{title}</p>
+        </li>
+        <div className="ml-2">
+          {status === 0 && (
             <button
               className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-xs px-2 py-1 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
               type="button"
@@ -26,14 +28,15 @@ const Todo = ({ dispatch, id, title, description, status, createdAt }) => {
             >
               Delete
             </button>
-          </span>
-        )}
-      </li>
-      <DetailModal
-        showModal={showModal}
-        setShowModal={setShowModal}
-        data={{ id, title, description, createdAt, status }}
-      />
+          )}
+        </div>
+
+        <DetailModal
+          showModal={showModal}
+          setShowModal={setShowModal}
+          data={{ id, title, description, createdAt, status }}
+        />
+      </div>
     </>
   );
 };
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,25 +1,43 @@
 import React from "react";
 import Todo from "./Todo";
 
+const DoneComponent = ({ todos }) => {
+  todos = todos.sort(
+    (a, b) => Date.parse(b.createdAt) - Date.parse(a.createdAt)
+  );
+  return (
+    <ul className="list-disc">
+      {todos.map((todo, idx) => (
+        <div key={idx}>{todo.status === 1 && <Todo {...todo} />}</div>
+      ))}
+    </ul>
+  );
+};
+
+const OngoingComponent = ({ todos }) => {
+  todos = todos.sort(
+    (a, b) => Date.parse(a.createdAt) - Date.parse(b.createdAt)
+  );
+  return (
+    <ul className="list-disc">
+      {todos.map((todo, idx) => (
+        <div key={idx}>{todo.status === 0 && <Todo {...todo} />}</div>
+      ))}
+    </ul>
+  );
+};
+
 const TodoList = ({ todos }) => {
   if (todos.length < 1) return <h1>loading</h1>;
   return (
     <div className="flex flex-row">
       <div className="basis-1/2">
-        <h3>Done</h3>
-        <ul className="list-disc">
-          {todos.map((todo, idx) => (
-            <div key={idx}>{todo.status === 1 && <Todo {...todo} />}</div>
-          ))}
-        </ul>
+        <h3>Task Done:</h3>
+        <DoneComponent todos={todos} />
       </div>
       <div className="basis-1/2">
-        <h3>Ongoing</h3>
-        <ul className="list-disc">
-          {todos.map((todo, idx) => (
-            <div key={idx}>{todo.status === 0 && <Todo {...todo} />}</div>
-          ))}
-        </ul>
+        <h3>Task Ongoing:</h3>
+        <OngoingComponent todos={todos} />
       </div>
     </div>
   );
